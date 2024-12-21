d8a8ff3f7100565401c91098b9751f5518eeaa46
GGhiaroni <gghiaronitiziano@gmail.com>
2024-12-21 00:44:23 -0300

    desenvolvimento funcionalidade de inserir novas tags no projeto

--- scripts.js
+++ scripts.js
@@ -36,3 +36,19 @@ uploadInput.addEventListener('change', async (e) => {
         }
     }
 });
+
+const inputTags = document.getElementById("input-tags");
+const listaDeTags = document.getElementById("lista-tags");
+
+inputTags.addEventListener('keypress', (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        const tagTexto = inputTags.value.toLowerCase().trim();
+        if (tagTexto !== "") {
+            const novaTag = document.createElement("li");
+            novaTag.classList.add("flex", "bg-corSecundaria", "text-corBackground", "gap-2", "py-1", "px-2", "rounded-md");
+            novaTag.innerHTML = `<p>${tagTexto}</p> <img src="./img/close-black.svg"/>`;
+            listaDeTags.appendChild(novaTag);
+        }
+    }
+});
